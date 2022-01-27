2cee455f526d10d2fd48ca2fde3645e135048800
Izhan <babuizhan1234@gmail.com>
2022-01-27 14:52:19 +0530

    Added shadows

--- components/projectCard.js
+++ components/projectCard.js
@@ -1,7 +1,7 @@
 const projectCard = ({ data }) => {
   return (
-    <div className="relative group h-full cursor-pointer">
-      <div className="absolute -inset-1 bg-gradient-to-r from-pink-600 to-purple-600 rounded-xs blur-sm opacity-50 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-gradient"></div>
+    <div className="relative group h-full cursor-pointer shadow-md hover:shadow-xl transition duration-300 dark:shadow-none">
+      <div className="absolute -inset-1 bg-gradient-to-r from-pink-600 to-purple-600 rounded-xs blur-sm opacity-50 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-gradient invisible dark:visible"></div>
       <div className="h-full w-full flex flex-col flex-grow flex-shrink relative">
         <a
           href={data.url}
@@ -9,7 +9,7 @@ const projectCard = ({ data }) => {
           target="_blank"
           rel="noreferrer"
         >
-          <div className="flex items-start justify-between flex-col bg-white dark:bg-gray-800 rounded-sm overflow-hidden shadow p-6">
+          <div className="flex items-start justify-between flex-col bg-white dark:bg-gray-800 rounded-sm overflow-hidden p-6">
             <div>
               <h1 className=" font-bold text-xl text-gray-800 dark:text-white">
                 {data.name}
