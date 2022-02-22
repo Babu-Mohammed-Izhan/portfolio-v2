94f1a598174e9170f38371e95597dabb443d633e
Izhan <babuizhan1234@gmail.com>
2022-02-22 20:32:02 +0530

    image

--- pages/index.js
+++ pages/index.js
@@ -33,6 +33,7 @@ export default function Home() {
           content="Full Stack Developer | Software Engineer | Computer Science Student"
         />
         <meta
+          name="image"
           property="og:image"
           content="https://res.cloudinary.com/dm8ogh4lv/image/upload/v1645541152/posrtfolioImages/Izhan_1_znlyzd.png"
         />
@@ -45,6 +46,7 @@ export default function Home() {
           content="Full Stack Developer | Software Engineer | Computer Science Student"
         />
         <meta
+          name="image"
           property="twitter:image"
           content="https://res.cloudinary.com/dm8ogh4lv/image/upload/v1645541152/posrtfolioImages/Izhan_1_znlyzd.png"
         />
