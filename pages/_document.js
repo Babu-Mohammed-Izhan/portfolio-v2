8c473e31c99650a4e15539806b221ca7111626b1
Izhan <babuizhan1234@gmail.com>
2022-03-23 16:08:14 +0530

    Added google analytics

---
+++ pages/_document.js
@@ -0,0 +1,39 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=G-W2Q55DG0Y8`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                                window.dataLayer = window.dataLayer || [];
+                                function gtag(){dataLayer.push(arguments);}
+                                gtag('js', new Date());
+                                gtag('config', 'G-W2Q55DG0Y8', {
+                                page_path: window.location.pathname,
+                                });
+                            `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
