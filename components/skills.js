3ba604822c37f5dbc104a9d9257bef14040692d9
Babu Izhan <babu.izhan@shopup.org>
2025-03-17 16:47:01 +0530

    feat: modify projects

--- components/skills.js
+++ components/skills.js
@@ -1,87 +1,78 @@
-import SkillCard from './skillCard';
+import SkillCard from "./skillCard";
 
 const Skill = () => {
   const icons = [
     {
-      name: 'HTML',
-      icon: 'https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643615636/posrtfolioImages/html-5_enugya.png',
+      name: "Typescript",
+      icon: "https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643616235/posrtfolioImages/typescript-icon_rahkor.png",
     },
     {
-      name: 'CSS',
-      icon: 'https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643636342/posrtfolioImages/css-3_su3dzj-removebg-preview_rrxxju.png',
+      name: "Javascript",
+      icon: "https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643615638/posrtfolioImages/javascript_l4fwtj.png",
     },
     {
-      name: 'SCSS',
-      icon: 'https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643615638/posrtfolioImages/sass_xwasyi.png',
+      name: "React",
+      icon: "https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643615638/posrtfolioImages/react_qlp3lp.png",
     },
     {
-      name: 'Bootstrap',
-      icon: 'https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643636279/posrtfolioImages/184-1844911_bootstrap-bootstrap-4-logo-png-removebg-preview_yedaxs-removebg-preview_f6zz7y.png',
+      name: "Redux",
+      icon: "https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643615638/posrtfolioImages/redux_tbj81x.png",
     },
     {
-      name: 'Tailwind',
-      icon: 'https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643616442/posrtfolioImages/Tailwind_CSS_Logo.svg_spbpio.png',
+      name: "Next",
+      icon: "https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643618266/posrtfolioImages/68747470733a2f2f73656261737469616e2d676f6d657a2e636f6d2f6e6578746a732e706e67_tvv8ml.png",
     },
     {
-      name: 'Javascript',
-      icon: 'https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643615638/posrtfolioImages/javascript_l4fwtj.png',
+      name: "React Native",
+      icon: "https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643615638/posrtfolioImages/react_qlp3lp.png",
     },
     {
-      name: 'Typescript',
-      icon: 'https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643616235/posrtfolioImages/typescript-icon_rahkor.png',
+      name: "Flutter",
+      icon: "https://res.cloudinary.com/dm8ogh4lv/image/upload/v1742210067/pngwing.com_asp5cc.png",
     },
     {
-      name: 'React',
-      icon: 'https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643615638/posrtfolioImages/react_qlp3lp.png',
+      name: "Express",
+      icon: "https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643618758/posrtfolioImages/png-transparent-express-js-node-js-javascript-mongodb-node-js-text-trademark-logo-removebg-preview-removebg-preview_wj0gkr.png",
     },
     {
-      name: 'Redux',
-      icon: 'https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643615638/posrtfolioImages/redux_tbj81x.png',
+      name: "HTML",
+      icon: "https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643615636/posrtfolioImages/html-5_enugya.png",
     },
     {
-      name: 'Next',
-      icon: 'https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643618266/posrtfolioImages/68747470733a2f2f73656261737469616e2d676f6d657a2e636f6d2f6e6578746a732e706e67_tvv8ml.png',
+      name: "CSS",
+      icon: "https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643636342/posrtfolioImages/css-3_su3dzj-removebg-preview_rrxxju.png",
     },
     {
-      name: 'Express',
-      icon: 'https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643618758/posrtfolioImages/png-transparent-express-js-node-js-javascript-mongodb-node-js-text-trademark-logo-removebg-preview-removebg-preview_wj0gkr.png',
+      name: "SCSS",
+      icon: "https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643615638/posrtfolioImages/sass_xwasyi.png",
     },
     {
-      name: 'NodeJS',
-      icon: 'https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643618358/posrtfolioImages/430-4309640_js-logo-nodejs-logo-clipart_uicjtp.png',
+      name: "Tailwind",
+      icon: "https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643616442/posrtfolioImages/Tailwind_CSS_Logo.svg_spbpio.png",
     },
     {
-      name: 'MongoDB',
-      icon: 'https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643779653/posrtfolioImages/MongoDB-Emblem-removebg-preview_kf5cau_1_atdfel.png',
+      name: "NodeJS",
+      icon: "https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643618358/posrtfolioImages/430-4309640_js-logo-nodejs-logo-clipart_uicjtp.png",
     },
     {
-      name: 'Postgres',
-      icon: 'https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643618050/posrtfolioImages/745px-Postgresql_elephant.svg_iusf8f.png',
-    },
-
-    {
-      name: 'GraphQl',
-      icon: 'https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643615638/posrtfolioImages/graphql_cpfhgn.png',
+      name: "MongoDB",
+      icon: "https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643779653/posrtfolioImages/MongoDB-Emblem-removebg-preview_kf5cau_1_atdfel.png",
     },
     {
-      name: 'Apollo GraphQl',
-      icon: 'https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643635839/posrtfolioImages/apollo-graphql_huswgi.png',
+      name: "Postgres",
+      icon: "https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643618050/posrtfolioImages/745px-Postgresql_elephant.svg_iusf8f.png",
     },
     {
-      name: 'Jest',
-      icon: 'https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643615638/posrtfolioImages/jest_faapi1.png',
+      name: "Jest",
+      icon: "https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643615638/posrtfolioImages/jest_faapi1.png",
     },
     {
-      name: 'Mocha',
-      icon: 'https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643616923/posrtfolioImages/mocha-2-logo-png-transparent_yhct3k.png',
+      name: "Python",
+      icon: "https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643615637/posrtfolioImages/python_llqx40.png",
     },
     {
-      name: 'Chai',
-      icon: 'https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643616911/posrtfolioImages/1515293_idifsi.png',
-    },
-    {
-      name: 'Python',
-      icon: 'https://res.cloudinary.com/dm8ogh4lv/image/upload/v1643615637/posrtfolioImages/python_llqx40.png',
+      name: "Go",
+      icon: "https://res.cloudinary.com/dm8ogh4lv/image/upload/v1742210069/pngwing.com_1_gaovld.png",
     },
   ];
 
